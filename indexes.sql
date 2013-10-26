4d4f934f73665a9a291a9e37d62f9649145cccc0
Sean Scott <oracle.sean@gmail.com>
2013-10-26 15:54:07 -0600

    Update indexes.sql

--- indexes.sql
+++ indexes.sql
@@ -14,23 +14,27 @@
     You should have received a copy of the GNU General Public License along
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
+     Report information on indexes for a given table.
+     
+     Oracle hasn't taken their own advice regarding the LONG datatype and
+     is still using it in DBA_IND_EXPRESSIONS, else I would have wrapped the
+     contents of function-based indexes into the column_name column with a
+     decode. One day I'll address the technical debt and write up something
+     to handle this. */
 
---Comprehensive report of all indexes for a given table.
 set lines 265
 column column_name format a30
 column segment_size format 999,999 heading SIZE
 column tablespace_name format a25
 column column_expression format a40 heading ''
 column clustering_factor heading CLUSTERING
-break on index_name on segment_size on uniqueness on index_type on visibility on tablespace_name on pct_free on blevel on leaf_blocks on distinct_keys on num_rows on clustering_factor
+break on index_name on segment_size on uniqueness on index_type on visibility on tablespace_name on blevel on leaf_blocks on distinct_keys on num_rows on clustering_factor
   select c.index_name
-,        s.bytes/1024/1024        as segment_size
+,        s.bytes/1024/1024             as segment_size
 ,        i.uniqueness
 ,        i.index_type
 ,        i.visibility
 ,        i.tablespace_name
-,        i.pct_free
 ,        i.blevel
 ,        i.leaf_blocks
 ,        i.distinct_keys
@@ -38,20 +42,20 @@ break on index_name on segment_size on uniqueness on index_type on visibility on
 ,        i.clustering_factor
 ,        c.column_name
 ,        e.column_expression
-    from dba_ind_columns           c
-,        dba_ind_expressions       e
-,        dba_segments              s
-,        dba_indexes               i
-   where i.table_owner             = upper('&owner')
-     and i.table_name              = upper('&table')
-     and i.owner                   = s.owner
-     and i.index_name              = s.segment_name
-     and i.owner                   = c.index_owner
-     and i.index_name              = c.index_name
-     and s.segment_type            = 'INDEX'
-     and c.index_owner             = e.index_owner (+)
-     and c.index_name              = e.index_name (+)
-     and c.column_position         = e.column_position (+)
+    from dba_ind_columns                c
+,        dba_ind_expressions            e
+,        dba_segments                   s
+,        dba_indexes                    i
+   where i.table_owner                  = upper('&OWNER')
+     and i.table_name                   = upper('&TABLE')
+     and i.owner                        = s.owner
+     and i.index_name                   = s.segment_name
+     and i.owner                        = c.index_owner
+     and i.index_name                   = c.index_name
+     and s.segment_type                 = 'INDEX'
+     and c.index_owner                  = e.index_owner (+)
+     and c.index_name                   = e.index_name (+)
+     and c.column_position              = e.column_position (+)
 order by i.index_name
 ,        c.column_position;
 clear breaks
